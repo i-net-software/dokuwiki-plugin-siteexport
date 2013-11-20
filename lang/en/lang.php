3c55fc88d62acfdf4b400562bd5a5542b681e237
Gerry Weißbach <gerry.w@gammaproduction.de>
2013-11-20 16:12:10 +0100

    Add mising settings localization

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -50,7 +50,7 @@ $lang['exportLinkedPages'] = 'Export Linked Pages';
 
 $lang['canOverwriteExisting'] = 'Overwrite existing Cron Job:';
 $lang['cronSaveProcess'] = 'Save as Cron Job';
-$lang['cronDescription'] = 'This allows to create cron-based jobs. They will be executed accoring to the server setting (you need command line access to the cron tool).';
+$lang['cronDescription'] = 'This allows to create cron-based jobs. They will be executed according to the server setting (you need command line access to the cron tool).';
 $lang['cronSaveAction'] = 'Save as Cron Job';
 $lang['cronDeleteAction'] = 'Delete Cron Job';
 
