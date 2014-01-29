e60b04c068f7524229473cf37f8829a984895fde
Alain Becker <alain@alain-becker.com>
2014-01-29 16:05:17 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Alain Becker <alain@alain-becker.com>
+ */
+$lang['depthType']             = 'Type d\'export';
+$lang['depth.pageOnly']        = 'cette page uniquement';
+$lang['depth']                 = 'Profondeur';
+$lang['exportLinkedPages']     = 'Exporter les pages liées';
+$lang['customOptions']         = 'Options personnalisées';
+$lang['search']                = 'Chercher';
+$lang['AggregateSubmitLabel']  = 'Télécharger';
