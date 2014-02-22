dd345c81285540157eaebb48d49079f5b4004129
Rene <wllywlnt@yahoo.com>
2014-02-22 12:48:43 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['debugLevel_o_5']        = 'Geen';
+$lang['debugLevel_o_4']        = 'Fout';
+$lang['debugLevel_o_3']        = 'Waarschuwing';
+$lang['debugLevel_o_2']        = 'Info';
+$lang['debugLevel_o_1']        = 'Debug';
