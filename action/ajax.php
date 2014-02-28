84d6549728735d3161495012a879efa8c79dfac1
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-02-28 08:57:46 +0100

    Only add parameters to final file when set by option

--- action/ajax.php
+++ action/ajax.php
@@ -39,17 +39,6 @@ class action_plugin_siteexport_ajax extends DokuWiki_Action_Plugin
     private $namespace = '';
 
     /**
-     * for backward compatability
-     * @see inc/DokuWiki_Plugin#getInfo()
-     */
-    function getInfo(){
-        if ( method_exists(parent, 'getInfo')) {
-            $info = parent::getInfo();
-        }
-        return is_array($info) ? $info : confToHash(dirname(__FILE__).'/../plugin.info.txt');
-    }
-
-    /**
      * Register Plugin in DW
      **/
     function register(&$controller) {
@@ -531,15 +520,14 @@ class action_plugin_siteexport_ajax extends DokuWiki_Action_Plugin
 			$this->functions->debug->message("Will replace old filename '{$fileName}' with {$tmpFile[2]}", null, 1);
         	$extension = array_pop(explode('.', $fileName));
 			$fileName = $dirname . '/' .  $this->functions->getSiteTitle($ID) . '.' . $extension;
-			$this->fileChecked[$url] = $fileName;
         } else if ( !empty($tmpFile[1]) && !strstr($DATA[2], $tmpFile[1]) ) {
         
 			$this->functions->debug->message("Will replace old filename '{$fileName}' with {$tmpFile[1]}", null, 1);
 			$fileName = $dirname . '/' . $tmpFile[1];
-			$this->fileChecked[$url] = $fileName;
         }
 
         // Add to zip
+		$this->fileChecked[$url] = $fileName;
         $status = $this->filewriter->__addFileToZip($tmpFile[0], $fileName);
         @unlink($tmpFile[0]);
 
@@ -946,7 +934,6 @@ class action_plugin_siteexport_ajax extends DokuWiki_Action_Plugin
 
         // 2010-10-23 - What if this is a fetch.php? than we produced an error.
         //        $this->fileChecked[] = $DATA[2];
-        $this->fileChecked[$url] = $DATA[2]; // 2010-09-03 - One URL to one FileName
 
         // get tempFile and save it
         $origDepth = $this->functions->settings->depth;
@@ -982,7 +969,8 @@ class action_plugin_siteexport_ajax extends DokuWiki_Action_Plugin
         $dirname = dirname($DATA[2]);
 
         // If a Filename was given that does not comply to the original name, us this one!
-        if ( !empty($tmpFile[1]) && !strstr($DATA[2], $tmpFile[1]) ) {
+        // 2014-02-28 But only if we are on PDF Mode. Does this produce any other Problems?
+        if ( $this->filewriter->canDoPDF() && !empty($tmpFile[1]) && !strstr($DATA[2], $tmpFile[1]) ) {
 			$DATA[2] = $dirname . '/' . $tmpFile[1];
         }
 
@@ -1005,7 +993,7 @@ class action_plugin_siteexport_ajax extends DokuWiki_Action_Plugin
 
         // depth is set, skip this one
         if ( is_null( $DEPTH ) ) $DEPTH = $this->functions->settings->depth;
-        $DATA[2] .= ( !empty( $DATA['PARAMS']) ? '?' . $DATA['PARAMS'] : '' ) . ( !empty( $DATA['ANCHOR'] ) ? '#' . $DATA['ANCHOR'] : '' );
+        $DATA[2] .= ( !empty( $DATA['PARAMS']) && $this->functions->settings->addParams? '?' . $DATA['PARAMS'] : '' ) . ( !empty( $DATA['ANCHOR'] ) ? '#' . $DATA['ANCHOR'] : '' );
 
         $newURL = $DATA[1] == 'url' ? $DATA[1] . '(' . $DEPTH . $DATA[2] . ')' : $DATA[1] . '="' . $DEPTH . $DATA[2] . '"';
         $this->functions->debug->message("Re-created URL: '$newURL'", null, 2);
