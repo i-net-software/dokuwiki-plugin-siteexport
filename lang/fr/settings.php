78b82c6e41327630546af1d823648308ea7b305d
NICOLAS ORTH <nicolas.orth@gmail.com>
2014-03-08 16:30:42 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,8 +4,10 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Alain Becker <alain@alain-becker.com>
+ * @author NICOLAS ORTH <nicolas.orth@gmail.com>
  */
 $lang['depth']                 = 'Profondeur d\'export par défaut';
+$lang['skipacl']               = 'Essayez d\'exporter des pages non-public';
 $lang['debugLevel_o_5']        = 'Aucun';
 $lang['debugLevel_o_4']        = 'Error';
 $lang['debugLevel_o_3']        = 'Warn';
