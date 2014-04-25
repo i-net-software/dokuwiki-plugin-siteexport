5472512bffde4d39604ff02ca3a3aedc17498122
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-04-25 10:31:34 +0200

    Two Step - System for Aggregator
    The Aggregator needs to prepare the page prior to any output. That is why it hooks before the header send event and prepares the page and metadata. It creates a temporary metadata page and removes it at the end.

--- action/aggregate.php
+++ action/aggregate.php
@@ -15,41 +15,70 @@ require_once(DOKU_PLUGIN.'action.php');
 class action_plugin_siteexport_aggregate extends DokuWiki_Action_Plugin {
 
 
+    private $instructions = null;
+    private $originalID = null;
+
     /**
 	* Register Plugin in DW
 	**/
 	function register(&$controller) {
+		$controller->register_hook('ACTION_HEADERS_SEND', 'BEFORE',  $this, 'siteexport_aggregate_prepare');
 		$controller->register_hook('TPL_ACT_RENDER', 'BEFORE',  $this, 'siteexport_aggregate');
 		$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'siteexport_aggregate_button', array ());
+	}
+	
+	function siteexport_aggregate_prepare(&$event)
+	{
+	    global $ID, $INFO;
+	    
+		if ( !isset($_REQUEST['siteexport_aggregate']) ) { return true; }
+		
+		$exportBase = cleanID($_REQUEST['baseID']);
+        $functions = plugin_load('helper', 'siteexport');
+        $values = $functions->__getOrderedListOfPagesForID(getNs($exportBase), $exportBase);
+    	
+    	$this->originalID = (string) $ID;
+
+        // Generate a TOC that can be exported
+        $TOC = "~~NOCACHE~~\n<toc merge mergeheader>\n";
+        $thema = array();
+        foreach( $values as $value ) {
+        	list($id, $title, $sort) = $value;
+
+        	$thema[] = p_get_metadata($id, 'thema', METADATA_RENDER_USING_SIMPLE_CACHE);
+        	$TOC .= "  * [[{$id}|{$title}]]\n";
+        }
+        
+        $TOC .= "</toc>";
+        
+        // Only get first and last element
+        $thema = array(reset($thema), end($thema));
+        
+        $meta = p_read_metadata($ID);
+        $ID = (string) cleanID($ID . '-toc-' . implode('-', array_filter($thema)));
+        
+        $meta['current']['thema'] = implode(' - ', array_filter($thema));
+        p_save_metadata($ID, $meta);
+
+        $this->instructions = $TOC;        
 	}
 	
 	function siteexport_aggregate(&$event)
 	{
 		global $ID, $INFO;
 
-		//if (  $event->data != 'siteexport_aggregate' ) { return true; }
-		if (  !isset($_REQUEST['siteexport_aggregate']) ) { return true; }
+        if ( empty($this->instructions) ) { return true; }
         $event->preventDefault();
-		
-		$exportBase = cleanID($_REQUEST['baseID']);
-        $functions=& plugin_load('helper', 'siteexport');
-        $values = $functions->__getOrderedListOfPagesForID(getNs($exportBase), $exportBase);
         
-        // Generate a TOC that can be exported
-        $TOC = "~~NOCACHE~~\n<toc merge mergeheader>\n";
-        foreach( $values as $value ) {
-        	list($id, $title, $sort) = $value;
-        	$TOC .= "  * [[{$id}|{$title}]]\n";
-        }
-        
-        $TOC .= "</toc>";
-        
-        $info = null;
-        $html = p_render('xhtml', p_get_instructions($TOC),$info);
+        $html = p_render('xhtml', p_get_instructions($this->instructions),$INFO);
         $html = html_secedit($html,false);
         if($INFO['prependTOC']) $html = tpl_toc(true).$html;
-        echo $html;
+
+        @unlink(metaFN($ID, '.meta'));
+
+        $ID = (string) $this->originalID;
         
+        echo $html;
         return false;
 	}
 	
