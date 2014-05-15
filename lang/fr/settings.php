fb50d2415966ca2678b3169a9b6acf5168e31323
Florian TASSIN <florian.tassin@gmail.com>
2014-05-15 14:10:08 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,9 +5,15 @@
  * 
  * @author Alain Becker <alain@alain-becker.com>
  * @author NICOLAS ORTH <nicolas.orth@gmail.com>
+ * @author Florian TASSIN <florian.tassin@gmail.com>
  */
 $lang['depth']                 = 'Profondeur d\'export par défaut';
 $lang['skipacl']               = 'Essayez d\'exporter des pages non-public';
+$lang['max_execution_time']    = 'Temps d\'exécution maximum pour un script';
+$lang['allowallusers']         = 'Autoriser tous les utilisateurs à exporter des pages';
+$lang['ignoreNon200']          = 'Ignorer les statuts HTTP autres que 200';
+$lang['debugFile']             = 'Fichier de débogage';
+$lang['debugLevel']            = 'Niveau de débogage';
 $lang['debugLevel_o_5']        = 'Aucun';
 $lang['debugLevel_o_4']        = 'Error';
 $lang['debugLevel_o_3']        = 'Warn';
