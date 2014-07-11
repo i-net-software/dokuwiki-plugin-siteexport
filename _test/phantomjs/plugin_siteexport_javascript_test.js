7c9cbdf531a70ac6965396552c149808ba1636e4
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-07-11 09:26:31 +0200

    Typo

--- _test/phantomjs/plugin_siteexport_javascript_test.js
+++ _test/phantomjs/plugin_siteexport_javascript_test.js
@@ -1,4 +1,4 @@
-test("JavaSCript did compile successfully", function(assert){
+test("JavaScript did compile successfully", function(assert){
    
    ok(jQuery, "jQuery does exist");
    ok(jQuery.siteexport().throbberCount == 0, "Siteexport does exist");
