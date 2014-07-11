6edd5922ea1142894f1dad2585c25ea14fc22d24
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-07-11 09:32:16 +0200

    Modify structure

--- _test/phantomjs/runPhantomJs.sh
+++ _test/phantomjs/runPhantomJs.sh
@@ -5,7 +5,7 @@ RET=0
 for FILE in $(find . -name "*.test.html")
 do
     echo "Running '$FILE' via phantomJS"
-    phantomjs phantom-qunit.js file://$(pwd)/$FILE
+    phantomjs phantom-qunit.js "file://$(pwd)/$FILE"
     RET=$(($RET + $?))
 done
 
