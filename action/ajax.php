9c10d86c379337d8e4f53a521c1dda54a0ebfa7d
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-10-13 09:33:04 +0200

    Improve non-200 ignorance.

--- action/ajax.php
+++ action/ajax.php
@@ -604,8 +604,9 @@ class action_plugin_siteexport_ajax extends DokuWiki_Action_Plugin
         
         if( $getData === false ) { // || ($http->status != 200 && !$this->functions->settings->ignoreNon200) ) {
         
-        	if ( $this->functions->settings->ignoreNon200 ) {
-	        	return false;
+        	if ( $http->status != 200 && $this->functions->settings->ignoreNon200 ) {
+                $this->functions->debug->message("HTTP status was '{$http->status}' - but I was told to ignore it by the settings.", $URL, 3);
+	        	return true;
         	}
         
             $this->functions->debug->message("Sending request failed with error, HTTP status was '{$http->status}'.", $URL, 4);
