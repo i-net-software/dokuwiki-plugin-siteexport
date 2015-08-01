df8d222b6b56e8afbfaed9adadae992007e7bea8
Hideaki SAWADA <chuno@live.jp>
2015-08-01 21:00:34 +0200

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['depth']                 = 'デフォルト出力階層数';
+$lang['skipacl']               = '非公開ページの出力を試みる';
+$lang['zipfilename']           = '出力 ZIP ファイルの Wiki パスと名前';
+$lang['exclude']               = '除外するページのパターン';
+$lang['max_execution_time']    = 'スクリプト実行時間の最大値';
+$lang['cachetime']             = '出力用のキャッシュ時間';
+$lang['allowallusers']         = 'ページ出力を全ユーザーに許可する';
+$lang['ignoreNon200']          = '出力時の 200 番代エラーを無視する';
+$lang['ignoreAJAXError']       = 'エラー時に出力を中断しない';
+$lang['debugFile']             = 'デバッグファイル';
+$lang['debugLevel']            = 'デバッグレベル';
+$lang['debugLevel_o_5']        = 'None';
+$lang['debugLevel_o_4']        = 'Error';
+$lang['debugLevel_o_3']        = 'Warn';
+$lang['debugLevel_o_2']        = 'Info';
+$lang['debugLevel_o_1']        = 'Debug';
+$lang['useOddEven']            = '奇数・偶数ページに異なるヘッダー・フッター設定を許可する。目次は常に奇数ページから始める。';
+$lang['defaultAuthenticationUser'] = '非公開ページ用のユーザー名';
+$lang['defaultAuthenticationPassword'] = '非公開ページ用のパスワード';
