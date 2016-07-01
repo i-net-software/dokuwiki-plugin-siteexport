14bb3a1b8c83e09d846adeb62b20617ecb2acae5
Myeongjin <aranet100@gmail.com>
2016-07-01 11:00:12 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['depth']                 = '기본 내보내기 깊이';
+$lang['skipacl']               = '비공개 문서 내보내기를 시도';
+$lang['zipfilename']           = '내보내는 ZIP 파일을 위한 위키 경로와 이름';
+$lang['exclude']               = '자료를 제외할 패턴';
+$lang['max_execution_time']    = '최대 스크립트 실행 시간';
+$lang['cachetime']             = '내보내기 위한 캐시 시간';
+$lang['allowallusers']         = '모든 사용자가 문서를 내보낼 수 있음';
+$lang['ignoreNon200']          = '내보내는 동안 200이 아닌 상태를 무시';
+$lang['ignoreAJAXError']       = '오류가 있는 내보내기를 중단하지 마십시오';
+$lang['debugFile']             = '디버그 파일';
+$lang['debugLevel']            = '디버그 수준';
+$lang['debugLevel_o_5']        = '없음';
+$lang['debugLevel_o_4']        = '오류';
+$lang['debugLevel_o_3']        = '경고';
+$lang['debugLevel_o_2']        = '정보';
+$lang['debugLevel_o_1']        = '디버그';
+$lang['useOddEven']            = '홀수/짝수 페이지에 다른 머릿글과 바닥글을 설정할 수 있습니다. 목차는 항상 홀수 페이지에 시작합니다.';
+$lang['defaultAuthenticationUser'] = '비공개 문서를 위한 사용자 이름';
+$lang['defaultAuthenticationPassword'] = '비공개 문서를 위한 비밀번호';
