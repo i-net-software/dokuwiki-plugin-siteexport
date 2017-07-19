a9fe5cc98ddcd9d5640d24fe3d72ba1c41f0605c
Tor Härnqvist <tor@harnqvist.se>
2017-07-19 19:55:34 +0200

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['depth']                 = 'Exportdjup som standard';
+$lang['skipacl']               = 'Försök att exportera icke-publika sidor';
+$lang['zipfilename']           = 'Wiki-sökväg och namn på exporterad ZIP-fil';
+$lang['exclude']               = 'Mönster för att exkludering av resurser/tillgångar';
+$lang['max_execution_time']    = 'Maximal tid för exekvering av manus';
+$lang['cachetime']             = 'Spara tid för exekvering av manus i cacheminne';
+$lang['allowallusers']         = 'Tillåt alla användare att exportera sidor';
+$lang['ignoreNon200']          = 'Ignorera icke-200-status under export';
+$lang['ignoreAJAXError']       = 'Stör inte exporten med fel';
+$lang['debugFile']             = 'Avlusningsfil';
+$lang['debugLevel']            = 'Avlusningsnivå';
+$lang['debugLevel_o_5']        = 'Ingen';
+$lang['debugLevel_o_4']        = 'Fel';
+$lang['debugLevel_o_3']        = 'Varna';
+$lang['debugLevel_o_2']        = 'Info';
+$lang['debugLevel_o_1']        = 'Avlusa';
+$lang['useOddEven']            = 'Tillåt olika sidhuvuden och sidfötter på udde/jämna sidor. Innehållsförteckning inleds alltid på udda sidnummer.';
+$lang['defaultAuthenticationUser'] = 'Användarnamn för icke-publika sidor';
+$lang['defaultAuthenticationPassword'] = 'Lösenord för icke-publika sidor';
