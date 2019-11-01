3f36cd5ce6b898b907a1ea812530ff537bf22d66
https://www.ecwid.com/store/theadsy/ <lorrineward@gawab.com>
2019-11-01 04:40:27 +0100

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -3,54 +3,162 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author https://www.ecwid.com/store/theadsy/ <lorrineward@gawab.com>
  * @author Myeongjin <aranet100@gmail.com>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['menu']                  = '사이트 내보내기';
-$lang['absolutePath']          = '절대 경로 내보내기';
-$lang['pdfExport']             = 'PDF 내보내기';
-$lang['usenumberedheading']    = '번호가 매겨진 문단 제목';
-$lang['template']              = '템플릿 내보내기';
-$lang['exportBody']            = '본문만 내보내기';
-$lang['addParams']             = '모든 변수 내보내기 (예로 "do")';
-$lang['disableCache']          = '현재 요청에 대한 캐시 비활성화';
-$lang['startingNamespace']     = '시작하는 이름공간을 입력하세요';
-$lang['selectYourOptions']     = '옵션을 선택하세요';
+$lang['encoding']              = 'Magnificent goods from you, man. I have understand 
+your stuff previous to and you are just too magnificent.
+
+I really like what you have acquired here, certainly like what you are saying and 
+the way in which you say it. You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is 
+actually a wonderful website.';
+$lang['direction']             = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+
+I really like what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care 
+of to keep it smart. I can\'t wait to read much more from 
+you. This is actually a wonderful website.';
+$lang['menu']                  = 'Viry-Chatillon';
+$lang['absolutePath']          = 'Viry-Chatillon';
+$lang['pdfExport']             = 'Viry-Chatillon';
+$lang['usenumberedheading']    = 'Magnificent goods from you, man. I have understand your stuff previous to and you 
+are just too magnificent. I really like what 
+you have acquired here, certainly like what you are saying and 
+the way in which you say it. You make it entertaining and you still take care of to keep 
+it smart. I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['template']              = 'Viry-Chatillon';
+$lang['exportBody']            = 'Viry-Chatillon';
+$lang['addParams']             = 'Viry-Chatillon';
+$lang['disableCache']          = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like 
+what you are saying and the way in which you say it.
+
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['startingNamespace']     = 'Ward';
+$lang['selectYourOptions']     = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too 
+magnificent. I really like what you have acquired here, certainly like what you are saying and the way in which you say 
+it. You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful 
+website.';
 $lang['helpCreationOptions']   = '도움말 만들기 옵션 중 하나를 선택하세요 (선택 사항)';
-$lang['eclipseDocZip']         = 'Eclipse 도움말 만들기';
-$lang['JavaHelpDocZip']        = 'Java 도움말 만들기';
-$lang['TOCMapWithoutTranslation'] = '번역 루트 제거';
-$lang['useTocFile']            = '이름공간에 목차 파일 사용';
+$lang['eclipseDocZip']         = '91170';
+$lang['JavaHelpDocZip']        = '91170';
+$lang['TOCMapWithoutTranslation'] = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['useTocFile']            = 'Lorrine';
 $lang['emptyTocElem']          = '목차에서의 빈 이름공간';
-$lang['startProcess']          = '시작 과정';
-$lang['directDownloadLink']    = '직접 다운로드 링크';
-$lang['wgetURLLink']           = 'wget 다운로드 URL';
-$lang['curlURLLink']           = 'curl 다운로드 URL';
-$lang['start']                 = '시작';
-$lang['status']                = '상태';
-$lang['ns']                    = '이름공간 설정';
-$lang['ens']                   = '내보낼 상위 이름공간';
-$lang['defaultLang']           = '다중 언어 이름공간에 대한 기본 언어';
-$lang['disablePluginsOption']  = '내보낼 때 (JS/CSS) 플러그인 비활성화';
-$lang['depthType']             = '내보내기 유형';
-$lang['depth.pageOnly']        = '이 문서만';
-$lang['depth.allSubNameSpaces'] = '모든 하위 이름공간';
-$lang['depth.specifiedDepth']  = '지정된 깊이';
-$lang['depth']                 = '깊이';
-$lang['renderer']              = '렌더 엔진';
-$lang['exportLinkedPages']     = '링크된 문서 내보내기';
-$lang['customOptions']         = '사용자 지정 옵션';
-$lang['customOptionsDescription'] = '내보내는 동안 고려되는 추가 사용자 지정 옵션을 추가할 수 있습니다';
-$lang['addCustomOption']       = '옵션 추가';
+$lang['startProcess']          = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are saying and the way in which you 
+say it. You make it entertaining and you still take care of to keep 
+it smart. I can\'t wait to read much more from you. This 
+is actually a wonderful website.';
+$lang['directDownloadLink']    = 'https://www.slideserve.com/account/dashboard.php';
+$lang['wgetURLLink']           = 'https://www.slideserve.com/account/dashboard.php';
+$lang['curlURLLink']           = 'https://www.slideserve.com/account/dashboard.php';
+$lang['start']                 = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+
+I really like what you have acquired here, certainly like what you are saying and 
+the way in which you say it. You make it entertaining and you still take care 
+of to keep it smart. I can\'t wait to read much more from you.
+This is actually a wonderful website.';
+$lang['status']                = 'Magnificent goods from you, man. I have understand your stuff previous to and 
+you are just too magnificent. I really like what you have acquired here, certainly 
+like what you are saying and the way in which you say it. You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['ns']                    = 'Magnificent goods from you, man. I have understand your stuff previous to and you 
+are just too magnificent. I really like what you have acquired here, 
+certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['ens']                   = 'Viry-Chatillon';
+$lang['defaultLang']           = 'Lorrine';
+$lang['disablePluginsOption']  = 'Viry-Chatillon';
+$lang['depthType']             = 'Viry-Chatillon';
+$lang['depth.pageOnly']        = 'Magnificent goods from you, man. I have understand your 
+stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly 
+like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['depth.allSubNameSpaces'] = 'Ward';
+$lang['depth.specifiedDepth']  = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+
+I really like what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['depth']                 = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it 
+smart. I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['renderer']              = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are saying and the way in which you say 
+it. You make it entertaining and you still take care of to keep it 
+smart. I can\'t wait to read much more from you. This 
+is actually a wonderful website.';
+$lang['exportLinkedPages']     = 'Viry-Chatillon';
+$lang['customOptions']         = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too 
+magnificent. I really like what you have acquired 
+here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually 
+a wonderful website.';
+$lang['customOptionsDescription'] = 'Viry-Chatillon';
+$lang['addCustomOption']       = 'Magnificent goods from you, man. I have understand your stuff previous to 
+and you are just too magnificent. I really like what you have acquired here, certainly like what you 
+are saying and the way in which you say it. You make it entertaining 
+and you still take care of to keep it smart. I 
+can\'t wait to read much more from you. This is actually a wonderful website.';
 $lang['search']                = '검색';
-$lang['toc']                   = '목차';
-$lang['AggregateSubmitLabel']  = '다운로드';
-$lang['AggragateExportPages']  = '병합을 위한 시작하는 문서:';
-$lang['toolbarButton']         = 'Siteexporter 넣기';
-$lang['useOptionsInEditor']    = '이러한 옵션을 사용하고 {{siteAggregator}} 요소 넣기';
-$lang['NoEntriesFoundHint']    = '집계를 위한 문서를 찾을 수 없습니다.';
-$lang['js']['loadingpage']     = '불러오는 중';
-$lang['js']['startdownload']   = '다운로드 시작 중';
-$lang['js']['downloadfinished'] = '다운로드 마침';
-$lang['js']['finishedbutdownloadfailed'] = '마쳤지만 다운로드에 실패했습니다.';
+$lang['toc']                   = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like 
+what you are saying and the way in which you say it.
+
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['AggregateSubmitLabel']  = 'Magnificent goods from you, man. I have understand your stuff 
+previous to and you are just too magnificent. I really like 
+what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['AggragateExportPages']  = 'Viry-Chatillon';
+$lang['SiteSubmitLabel']       = 'Magnificent goods from you, man. I have understand your stuff previous to and 
+you are just too magnificent. I really like what you have 
+acquired here, certainly like what you are saying and 
+the way in which you say it. You make it entertaining and you still take care 
+of to keep it smart. I can\'t wait to read much more from you.
+This is actually a wonderful website.';
+$lang['toolbarButton']         = 'Viry-Chatillon';
+$lang['useOptionsInEditor']    = 'Magnificent goods from you, man. I have understand your 
+stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a 
+wonderful website.';
+$lang['NoEntriesFoundHint']    = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are saying and the way 
+in which you say it. You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['siteexport_button']     = 'Viry-Chatillon';
+$lang['js']['loadingpage']     = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['js']['startdownload']   = 'Magnificent goods from you, man. I have understand your stuff previous to and 
+you are just too magnificent. I really like what you have acquired here, certainly like what you are saying and the way in which you say 
+it. You make it entertaining and you still take care of to keep it 
+smart. I can\'t wait to read much more from you. This is actually 
+a wonderful website.';
+$lang['js']['downloadfinished'] = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are 
+saying and the way in which you say it. You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['js']['finishedbutdownloadfailed'] = 'Magnificent goods from you, man. I have understand your stuff previous to 
+and you are just too magnificent. I really like what you have acquired here, certainly like what you are 
+saying and the way in which you say it. You make it entertaining and you still take 
+care of to keep it smart. I can\'t wait to read 
+much more from you. This is actually a wonderful website.';
--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -3,24 +3,81 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author https://www.ecwid.com/store/theadsy/ <lorrineward@gawab.com>
  * @author Myeongjin <aranet100@gmail.com>
  */
-$lang['depth']                 = '기본 내보내기 깊이';
-$lang['skipacl']               = '비공개 문서 내보내기를 시도';
-$lang['zipfilename']           = '내보내는 ZIP 파일을 위한 위키 경로와 이름';
-$lang['exclude']               = '자료를 제외할 패턴';
-$lang['max_execution_time']    = '최대 스크립트 실행 시간';
-$lang['cachetime']             = '내보내기 위한 캐시 시간';
-$lang['allowallusers']         = '모든 사용자가 문서를 내보낼 수 있음';
-$lang['ignoreNon200']          = '내보내는 동안 200이 아닌 상태를 무시';
-$lang['ignoreAJAXError']       = '오류가 있는 내보내기를 중단하지 마십시오';
-$lang['debugFile']             = '디버그 파일';
-$lang['debugLevel']            = '디버그 수준';
-$lang['debugLevel_o_5']        = '없음';
-$lang['debugLevel_o_4']        = '오류';
-$lang['debugLevel_o_3']        = '경고';
-$lang['debugLevel_o_2']        = '정보';
-$lang['debugLevel_o_1']        = '디버그';
-$lang['useOddEven']            = '홀수/짝수 페이지에 다른 머릿글과 바닥글을 설정할 수 있습니다. 목차는 항상 홀수 페이지에 시작합니다.';
-$lang['defaultAuthenticationUser'] = '비공개 문서를 위한 사용자 이름';
-$lang['defaultAuthenticationPassword'] = '비공개 문서를 위한 비밀번호';
+$lang['depth']                 = 'Viry-Chatillon';
+$lang['skipacl']               = 'Viry-Chatillon';
+$lang['zipfilename']           = 'Lorrine';
+$lang['exclude']               = 'Magnificent goods from you, man. I have understand your stuff previous 
+to and you are just too magnificent. I really like 
+what you have acquired here, certainly like what you are saying and the way in which you say it.
+
+You make it entertaining and you still take care 
+of to keep it smart. I can\'t wait to read much more from you.
+This is actually a wonderful website.';
+$lang['max_execution_time']    = 'Magnificent goods from you, man. I have understand your stuff previous to and you 
+are just too magnificent. I really like what you have acquired here,
+certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take 
+care of to keep it smart. I can\'t wait to read much more from you.
+This is actually a wonderful website.';
+$lang['cachetime']             = 'Viry-Chatillon';
+$lang['allowallusers']         = 'Viry-Chatillon';
+$lang['ignoreNon200']          = 'Viry-Chatillon';
+$lang['ignoreAJAXError']       = 'Viry-Chatillon';
+$lang['debugFile']             = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just 
+too magnificent. I really like what you have acquired here, 
+certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['debugLevel']            = 'Magnificent goods from you, man. I have understand 
+your stuff previous to and you are just too magnificent. I really like what 
+you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['debugLevel_o_5']        = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like what you are 
+saying and the way in which you say it. You make it entertaining 
+and you still take care of to keep it smart. I 
+can\'t wait to read much more from you. This is actually a wonderful website.';
+$lang['debugLevel_o_4']        = 'Magnificent goods from you, man. I have understand your stuff previous to and you are 
+just too magnificent. I really like what you have acquired here, certainly like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it 
+smart. I can\'t wait to read much more from you.
+This is actually a wonderful website.';
+$lang['debugLevel_o_3']        = 'Magnificent goods from you, man. I have understand your stuff 
+previous to and you are just too magnificent.
+I really like what you have acquired here, certainly 
+like what you are saying and the way in which you say it.
+You make it entertaining and you still take care of 
+to keep it smart. I can\'t wait to read much more from you.
+This is actually a wonderful website.';
+$lang['debugLevel_o_2']        = 'Magnificent goods from you, man. I have understand your 
+stuff previous to and you are just too magnificent. I really 
+like what you have acquired here, certainly like what you are saying and the way 
+in which you say it. You make it entertaining and you still take care of to 
+keep it smart. I can\'t wait to read much more from you.
+
+This is actually a wonderful website.';
+$lang['debugLevel_o_1']        = 'Magnificent goods from you, man. I have understand your stuff previous to and you are just 
+too magnificent. I really like what you have acquired here, certainly like what 
+you are saying and the way in which you say it.
+You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful 
+website.';
+$lang['useOddEven']            = 'Magnificent goods from you, man. I have understand your stuff 
+previous to and you are just too magnificent.
+I really like what you have acquired here, certainly like 
+what you are saying and the way in which you say it.
+
+You make it entertaining and you still take care of to keep 
+it smart. I can\'t wait to read much more from you. This is 
+actually a wonderful website.';
+$lang['defaultAuthenticationUser'] = 'Lorrine';
+$lang['defaultAuthenticationPassword'] = 'Magnificent goods from you, man. I have understand your stuff previous 
+to and you are just too magnificent. I really like what you have acquired here, 
+certainly like what you are saying and the way in which you say 
+it. You make it entertaining and you still take care of to keep it smart.
+I can\'t wait to read much more from you. This is actually a wonderful website.';
