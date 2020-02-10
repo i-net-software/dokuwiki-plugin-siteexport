01d694166ae291c45f749c74ead544f7e65acc9b
Download <fredricstainforth@freenet.de>
2020-02-11 00:10:34 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,76 +1,62 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * english language file
+ *
+ * @author Download <fredricstainforth@freenet.de>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'Site Export'; 
- 
-// custom language strings for the plugin
-$lang['absolutePath'] = 'Export Absolute Paths'; 
-$lang['pdfExport'] = 'PDF Export'; 
-$lang['usenumberedheading'] = 'Numbered Headings'; 
-$lang['template'] = 'Export Template'; 
-$lang['exportBody'] = 'Export Body only'; 
-$lang['addParams'] = 'Export all parameters (e.g. "do")';
-$lang['disableCache'] = 'Disable cache for current request';
-
-$lang['startingNamespace'] = 'Enter your starting Namespace'; 
-$lang['selectYourOptions'] = 'Select your Options'; 
-$lang['helpCreationOptions'] = 'Select one of the Help Creation Options (optional)'; 
-$lang['eclipseDocZip'] = 'Create Eclipse Help'; 
-$lang['JavaHelpDocZip'] = 'Create Java Help';
-$lang['TOCMapWithoutTranslation'] = 'Remove Translation Root';
-
-$lang['useTocFile'] = 'Use TOC file in Namespace'; 
-$lang['emptyTocElem'] = 'Empty Namespaces in TOC'; 
-$lang['startProcess'] = 'Start Process'; 
-$lang['directDownloadLink'] = 'Direct Download Link'; 
-$lang['wgetURLLink'] = 'wget Download URL';
-$lang['curlURLLink'] = 'curl Download URL';
-$lang['start'] = 'start'; 
-$lang['status'] = 'Status'; 
-$lang['ns'] = 'Set Namespace';
-$lang['ens'] = 'Parent Namespace to export';
-$lang['defaultLang'] = 'Default Language for multi-language namespaces';
-
-$lang['disablePluginsOption'] = 'Disable (JS/CSS) Plugins while export'; 
-
-$lang['depthType'] = 'Export Type';
-$lang['depth.pageOnly'] = 'this page only';
-$lang['depth.allSubNameSpaces'] = 'all sub namespaces';
-$lang['depth.specifiedDepth'] = 'specified depth';
-
-$lang['depth'] = 'Depth';
-$lang['renderer'] = 'Render Engine';
-
-$lang['exportLinkedPages'] = 'Export Linked Pages';
-
-$lang['customOptions'] = 'Custom Options';
-$lang['customOptionsDescription'] = 'You can add further custom options that will be considered while exporting';
-$lang['addCustomOption'] = 'add Option';
-
-$lang['search'] = 'Search';
-$lang['toc'] = 'Table of Contents';
-
-
-$lang['AggregateSubmitLabel'] = 'Download';
-$lang['AggragateExportPages'] = 'Starting page for merger:';
-$lang['SiteSubmitLabel'] = 'Download as PDF';
-$lang['toolbarButton'] = 'Insert Siteexporter';
-$lang['useOptionsInEditor'] = 'Use these options and insert {{siteAggregator}} element';
-$lang['NoEntriesFoundHint'] = 'No pages have been found for aggregation.';
-
-$lang['siteexport_button'] = 'Export Page to HTML/PDF';
-
-$lang['js']['loadingpage'] = 'Creating document';
-$lang['js']['startdownload'] = 'Starting Download';
-$lang['js']['downloadfinished'] = 'Finished Download';
-$lang['js']['finishedbutdownloadfailed'] = 'Finished but download failed.';
-
-//Setup VIM: ex: et ts=4 enc=utf-8 :
+$lang['encoding']              = 'I used to be able to find good info from your articles.';
+$lang['direction']             = 'I used to be able to find good info from your articles.';
+$lang['menu']                  = 'Bosschenhoofd';
+$lang['absolutePath']          = 'Bosschenhoofd';
+$lang['pdfExport']             = 'Bosschenhoofd';
+$lang['usenumberedheading']    = 'I used to be able to find good info from your articles.';
+$lang['template']              = 'Bosschenhoofd';
+$lang['exportBody']            = 'Bosschenhoofd';
+$lang['addParams']             = 'Bosschenhoofd';
+$lang['disableCache']          = 'I used to be able to find good info from your articles.';
+$lang['startingNamespace']     = 'Stainforth';
+$lang['selectYourOptions']     = 'I used to be able to find good info from your articles.';
+$lang['helpCreationOptions']   = 'Select one of the Help Creation Options (optional)';
+$lang['eclipseDocZip']         = '4744 Rr';
+$lang['JavaHelpDocZip']        = '4744 Rr';
+$lang['TOCMapWithoutTranslation'] = 'I used to be able to find good info from your articles.';
+$lang['useTocFile']            = 'Fredric';
+$lang['emptyTocElem']          = 'Empty Namespaces in TOC';
+$lang['startProcess']          = 'I used to be able to find good info from your articles.';
+$lang['directDownloadLink']    = 'https://www.download-pc-games.com';
+$lang['wgetURLLink']           = 'https://www.download-pc-games.com';
+$lang['curlURLLink']           = 'https://www.download-pc-games.com';
+$lang['start']                 = 'I used to be able to find good info from your articles.';
+$lang['status']                = 'I used to be able to find good info from your articles.';
+$lang['ns']                    = 'I used to be able to find good info from your articles.';
+$lang['ens']                   = 'Bosschenhoofd';
+$lang['defaultLang']           = 'Fredric';
+$lang['disablePluginsOption']  = 'Bosschenhoofd';
+$lang['depthType']             = 'Bosschenhoofd';
+$lang['depth.pageOnly']        = 'I used to be able to find good info from your articles.';
+$lang['depth.allSubNameSpaces'] = 'Stainforth';
+$lang['depth.specifiedDepth']  = 'I used to be able to find good info from your 
+articles.';
+$lang['depth']                 = 'I used to be able to find good info from your articles.';
+$lang['renderer']              = 'I used to be able to find good info from your articles.';
+$lang['exportLinkedPages']     = 'Bosschenhoofd';
+$lang['customOptions']         = 'I used to be able to find good info from your articles.';
+$lang['customOptionsDescription'] = 'Bosschenhoofd';
+$lang['addCustomOption']       = 'I used to be able to find good info from your articles.';
+$lang['search']                = 'Search';
+$lang['toc']                   = 'I used to be able to find good info from your articles.';
+$lang['AggregateSubmitLabel']  = 'I used to be able to find good info from your articles.';
+$lang['AggragateExportPages']  = 'Bosschenhoofd';
+$lang['SiteSubmitLabel']       = 'I used to be able to find good info from your articles.';
+$lang['toolbarButton']         = 'Bosschenhoofd';
+$lang['useOptionsInEditor']    = 'I used to be able to find good info from your articles.';
+$lang['NoEntriesFoundHint']    = 'I used to be able to find good info from your articles.';
+$lang['siteexport_button']     = 'Bosschenhoofd';
+$lang['js']['loadingpage']     = 'I used to be able to find good info from your articles.';
+$lang['js']['startdownload']   = 'I used to be able to find good info from your articles.';
+$lang['js']['downloadfinished'] = 'I used to be able to find good info from your articles.';
+$lang['js']['finishedbutdownloadfailed'] = 'I used to be able to find good info from your 
+articles.';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,29 +1,28 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * english language file
+ *
+ * @author Download <fredricstainforth@freenet.de>
  */
-$lang['depth'] = 'Default Export Depth';
-$lang['skipacl'] = 'Try to export non-public pages';
-$lang['zipfilename'] = 'Wiki Path and name for exported ZIP file';
-$lang['exclude'] = 'Pattern to exclude resources';
-$lang['max_execution_time'] = 'Maximum script execution time';
-$lang['cachetime'] = 'Cache time for export';
-
-$lang['allowallusers'] = 'Allow all users to export pages';
-$lang['ignoreNon200'] = 'Ignore non-200 status during export';
-$lang['ignoreAJAXError'] = 'Do not interrupt export with errors';
-$lang['debugFile'] = 'Debug File';
-
-$lang['debugLevel'] = 'Debug Level';
-$lang['debugLevel_o_5'] = 'None';
-$lang['debugLevel_o_4'] = 'Error';
-$lang['debugLevel_o_3'] = 'Warn';
-$lang['debugLevel_o_2'] = 'Info';
-$lang['debugLevel_o_1'] = 'Debug';
-
-$lang['useOddEven'] = 'Allows setting of different headers and footer on odd/even pages. TOCs will always start on odd pages.';
-
-$lang['defaultAuthenticationUser']        = 'Username for non-public pages';
-$lang['defaultAuthenticationPassword']    = 'Password for non-public pages';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['depth']                 = 'Bosschenhoofd';
+$lang['skipacl']               = 'Bosschenhoofd';
+$lang['zipfilename']           = 'Fredric';
+$lang['exclude']               = 'I used to be able to find good info from your articles.';
+$lang['max_execution_time']    = 'I used to be able to find good info from your articles.';
+$lang['cachetime']             = 'Bosschenhoofd';
+$lang['allowallusers']         = 'Bosschenhoofd';
+$lang['ignoreNon200']          = 'Bosschenhoofd';
+$lang['ignoreAJAXError']       = 'Bosschenhoofd';
+$lang['debugFile']             = 'I used to be able to find good info from your articles.';
+$lang['debugLevel']            = 'I used to be able to find good info from your articles.';
+$lang['debugLevel_o_5']        = 'I used to be able to find good info from your articles.';
+$lang['debugLevel_o_4']        = 'I used to be able to find good info from your articles.';
+$lang['debugLevel_o_3']        = 'I used to be able to find good info from your articles.';
+$lang['debugLevel_o_2']        = 'I used to be able to find good info from your articles.';
+$lang['debugLevel_o_1']        = 'I used to be able to find good info from your articles.';
+$lang['useOddEven']            = 'I used to be able to find good info from your articles.';
+$lang['defaultAuthenticationUser'] = 'Fredric';
+$lang['defaultAuthenticationPassword'] = 'I used to be able to find good info from your articles.';
