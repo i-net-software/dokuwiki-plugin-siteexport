90f9dedd289e5079ab9255442748577e897ee36a
Igor Banatov <igor.banatov@gmail.com>
2020-05-02 11:10:20 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Igor Banatov <igor.banatov@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'Экспорт Сайта';
+$lang['absolutePath']          = 'Экспорт абсолютных путей размещения';
+$lang['pdfExport']             = 'Экспорт в PDF';
+$lang['usenumberedheading']    = 'Нумерованные заголовки';
+$lang['template']              = 'Шаблон экспорта';
+$lang['exportBody']            = 'Экспорт только тела';
+$lang['addParams']             = 'Экспорт всех параметров (например "do")';
+$lang['disableCache']          = 'Выключить кэширование для текущего запроса';
+$lang['startingNamespace']     = 'Введите вашу стартовою именованную область ';
+$lang['selectYourOptions']     = 'Выберите ваши параметры';
+$lang['helpCreationOptions']   = 'Выберите одну из опций созадния Помощи (опционально)';
+$lang['eclipseDocZip']         = 'Создать Помощь для Eclipse ';
+$lang['JavaHelpDocZip']        = 'Создать Помощь для Java ';
+$lang['TOCMapWithoutTranslation'] = 'Удалить изначальный перевод';
+$lang['useTocFile']            = 'Использовать файл Содержания в именованной области';
+$lang['emptyTocElem']          = 'Пустые именованные области в Содержании';
+$lang['startProcess']          = 'Запустить процесс';
+$lang['directDownloadLink']    = 'Прямая ссылка для загрузки';
+$lang['wgetURLLink']           = 'wget ссылка для загрузки';
+$lang['curlURLLink']           = 'curl ссылка для загрузки';
+$lang['start']                 = 'Запуск';
+$lang['status']                = 'Статус';
+$lang['ns']                    = 'Задать именованную область';
+$lang['ens']                   = 'Родительская именовая область для экспорта';
+$lang['defaultLang']           = 'Язык по умолчанию для мульти=языковой именованной области';
+$lang['disablePluginsOption']  = 'Выключить JS/CSS плагин при экспорте';
+$lang['depthType']             = 'Тип экспорта';
+$lang['depth.pageOnly']        = 'только эту страницу';
+$lang['depth.allSubNameSpaces'] = 'все подчиненные именованные области';
+$lang['depth.specifiedDepth']  = 'указанной глубины';
+$lang['depth']                 = 'Глубина';
+$lang['renderer']              = 'Способ преобразования';
+$lang['exportLinkedPages']     = 'Экспорт связаных страниц';
+$lang['customOptions']         = 'Пользовательские опции';
+$lang['customOptionsDescription'] = 'Вы можете добавить свои пользовательские опции, которые будут использоваться при жкспорте';
+$lang['addCustomOption']       = 'добавить Опцию';
+$lang['search']                = 'Поиск';
+$lang['toc']                   = 'Таблица Содержания';
+$lang['AggregateSubmitLabel']  = 'Загрузка';
+$lang['AggragateExportPages']  = 'Начальная страница для слияния';
+$lang['SiteSubmitLabel']       = 'Загрузить в формате PDF';
+$lang['toolbarButton']         = 'Вставить Siteexporter';
+$lang['useOptionsInEditor']    = 'Использовать данные опции и вставить элементы {{siteAggregator}}  ';
+$lang['NoEntriesFoundHint']    = 'Не найдено станиц для аггрегирования';
+$lang['siteexport_button']     = 'Экспорт страницы в формате HTML/PDF';
+$lang['js']['loadingpage']     = 'Создание документа';
+$lang['js']['startdownload']   = 'Начало загрузки';
+$lang['js']['downloadfinished'] = 'Оконченная загрузка';
+$lang['js']['finishedbutdownloadfailed'] = 'Закончено, но загрузка неуспешна.';
---
+++ lang/ru/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Igor Banatov <igor.banatov@gmail.com>
+ */
+$lang['depth']                 = 'Глубина экспорта по умолчанию';
+$lang['skipacl']               = 'Пытаться экспортировать непубличные страницы ';
+$lang['zipfilename']           = 'Путь и имя WiKi файла для экспорта в ZIP файл ';
+$lang['exclude']               = 'Шаблон ля исключения источников';
+$lang['max_execution_time']    = 'Максимальное время выполнения скрипта';
+$lang['cachetime']             = 'Время кэширования для экспорта';
+$lang['allowallusers']         = 'Позволить всем пользователям экспортировать страницы';
+$lang['ignoreNon200']          = 'Игнорировать статусы non-200 во время экспорта';
+$lang['ignoreAJAXError']       = 'Не прерывать экспорт при ошибках';
+$lang['debugFile']             = 'Файл отладки';
+$lang['debugLevel']            = 'Уровень отладки';
+$lang['debugLevel_o_5']        = 'Нет';
+$lang['debugLevel_o_4']        = 'Ошибка';
+$lang['debugLevel_o_3']        = 'Предупреждение';
+$lang['debugLevel_o_2']        = 'Информация';
+$lang['debugLevel_o_1']        = 'Отладка';
+$lang['useOddEven']            = 'Разрешить разные заголовки верхних и нижних полей страницы на четных/нечетных страницах.';
+$lang['defaultAuthenticationUser'] = 'Имя пользователя для не-публичных страниц';
+$lang['defaultAuthenticationPassword'] = 'Пароль для не-публичных страниц';
