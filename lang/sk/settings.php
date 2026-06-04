f6836ec0ecf3bb4e602eadfa05b8bd178789a198
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:36:26 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['depth']                 = 'Predvolená hĺbka exportu';
+$lang['skipacl']               = 'Pokúsiť sa exportovať neverejné stránky';
+$lang['zipfilename']           = 'Wiki cesta a názov exportovaného ZIP súboru';
+$lang['exclude']               = 'Vzor na vylúčenie zdrojov';
+$lang['max_execution_time']    = 'Maximálny čas behu skriptu';
+$lang['cachetime']             = 'Čas cachovania exportu';
+$lang['allowallusers']         = 'Povoliť export stránok všetkým používateľom';
+$lang['ignoreNon200']          = 'Ignorovať iné než 200 stavové kódy počas exportu';
+$lang['ignoreAJAXError']       = 'Neprerušovať export pri chybách';
+$lang['debugFile']             = 'Debug súbor';
+$lang['debugLevel']            = 'Úroveň debugovania';
+$lang['debugLevel_o_5']        = 'Žiadna';
+$lang['debugLevel_o_4']        = 'Error';
+$lang['debugLevel_o_3']        = 'Warn';
+$lang['debugLevel_o_2']        = 'Info';
+$lang['debugLevel_o_1']        = 'Debug';
+$lang['useOddEven']            = 'Umožňuje nastaviť rôzne hlavičky a pätičky na nepárnych/párnych stranách. Obsahy budú vždy začínať na nepárnych stranách.';
+$lang['defaultAuthenticationUser'] = 'Používateľské meno pre neverejné stránky';
+$lang['defaultAuthenticationPassword'] = 'Heslo pre neverejné stránky';
